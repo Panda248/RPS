6d91566d27e2e2b7ce21c01eaf88d4b9589e602b
Panda248 <40081083+Panda248@users.noreply.github.com>
2019-07-13 16:14:51 -0400

    Add files via upload

---
+++ hiScript.js
@@ -0,0 +1,22 @@
+		var choices = ["Rock", "Paper", "Scissors"];
+		var plrInputStr;
+		var comInput;
+		var plrInputInt;
+function main() {
+	plrInputStr = document.getElementById("userInput");
+	console.log(plrInputStr);
+	plrInputInt = parseInt(plrInputStr);
+	console.log(plrInputInt);
+	comInput = Math.floor(Math.random() * 3) + 1;
+}
+function calcResult (x, y)	{
+	document.getElementsByClassName('Results');
+	switch (x,y) {
+		case x === 1 && y === 3:
+
+
+			break;
+		default:
+
+	}
+}
